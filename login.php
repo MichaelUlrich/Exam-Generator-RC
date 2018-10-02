9fcabf97b31f8c8174bd55212d7b757328133c4d
MichaelUlrich <mikeulrich3@gmail.com>
2018-10-02 17:37:37 -0400

    added PHP for login, still need URL

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+	$user = array('username' => $_POST['username'], 'password' =>$_POST['password']);
+        $middle_url = "https://web.njit.edu/~bkw2/middle.php";				//Receive if login is for student or teacher
+	$ch = curl_init();
+	       curl_setopt($ch, CURLOPT_URL, $middle_url);                             //Set URL
+	       curl_setopt($ch, CURLOPT_POST, true);                                   //Set to POST
+	       curl_setopt($ch, CURLOPT_POSTFIELDS, /*http_build_query*/ $user);       //Send array
+	       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                         //Receive results of cURL and store in var instead of printing
+	$result = curl_exec($ch);
+	$result_decode=json_decode($result);
+	if($result == false || $result == null) {
+		echo "No result";
+	} else {
+	        echo $result . "<br>";
+	}
+	curl_close($ch);
+?>
