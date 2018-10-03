bfb897bb26c13417ac91dca06a20cb33bd5c7a7d
MichaelUlrich <mikeulrich3@gmail.com>
2018-10-02 20:13:13 -0400

    added headers to create sessions for Instructors and Students

---
+++ studentHeader.php
@@ -0,0 +1,6 @@
+<?php
+	/* Header to include in all Student HTML&PHP files to ensure a valid student is logged in to access tests and grades */
+	y
+	session_start();
+
+?
---
+++ teacherHeader.php
@@ -0,0 +1,5 @@
+<?php
+	/* Header to include in all Teacher HTML&PHP files to ensure only an instructor can access test creation*/
+	session_start();
+
+?
