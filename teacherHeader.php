b64d7f060cc948c2c45e33a7d3a313c5c618213b
MichaelUlrich <mikeulrich3@gmail.com>
2018-10-02 21:13:02 -0400

    Updated header files and implemented header files

--- teacherHeader.php
+++ teacherHeader.php
@@ -1,5 +1,10 @@
 <?php
 	/* Header to include in all Teacher HTML&PHP files to ensure only an instructor can access test creation*/
 	session_start();
+	if(!isset($_SESSION['teacher'])) {
+		echo "You are not a teacher";
+	} else {
+		echo "Teacher, OK";
+	}
 
-?
+?>
