0aaee9ac5b383bf57d4ba6bbf6fa07433af1c27a
MichaelUlrich <mikeulrich3@gmail.com>
2018-10-16 19:27:13 -0400

    addQuestion now submits to database.  Need to implement ajax

--- addQuestion.php
+++ addQuestion.php
@@ -1,6 +1,21 @@
 <!DOCTYPE HTML>
 <html>
-<?php include 'teacherHeader.php'; ?>
+<?php 
+	$var = array('question' => $_POST['question'], 'answer' => $_POST['answer'], 'type' => $_POST['type'], 'difficulty' => $_POST['difficulty']);
+	$ch = curl_init("https://web.njit.edu/~bkw2/addQuestions.php");
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $var);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+	$result = curl_exec($ch);
+	echo $result;	
+
+?>
+<!--<script>
+	var = curlResult = "<?php echo $result; ?>" ;
+	document.getElementById('result').innerHTML = "HELLO";
+
+</script> -->
 <title> Add Question to Database </title>
 <head>
 <!-- Resize to fit screen dynamically -->
@@ -15,35 +30,41 @@
 </style>
 </head>
 <body>
+<script>
+       // var = curlResult = "<?php echo $result; ?>" ;
+	function moveText() {
+		document.getElementById("result").innerHTML = "HELLO";
+	}
+</script>
 	<h2> Choose Questions to Add to the Test </h2>
 	<div class="row">
 		<div class="column" style="background-color:#fff;">
 			<h2> Enter Question and Correct Output </h2>
-			<form>
+			<form method="post">
 				<p> Enter Question: </p>
-				<input type="text" name="question"><br>
+					<input type="text" name="question" id="question"><br>
 				<p> Enter Correct Answer </p>
-				<input type="text" name="answer"><br>
+					<input type="text" name="answer" id="answer"><br>
 				<p> Select Question Difficulty </p>
-				<select name="difficulty">
+				<select name="difficulty" id="difficulty">
 					<option value="easy">Easy</option>
 					<option value="medium">Medium</option>
 					<option value="hard">Hard</option>
 				</select>
 				<p> Select Question Type </p>
-				        <select name="type">
+				<select name="type" id="type">
 					<option value="loop">Loop</option>
 					<option value="function">Function</option>
 					<option value="variable">Variable</option>
 				</select>
-
-																											
+				<input type="submit" name="submit" onclick="moveText()">
 			</form>
 		</div>
 		<div class="column" style="background-color:#bbb;">
 			<h2> Added Questions </h2>
 			<p> SAMPLE TEXT </p>
 			<p> HAVE QUESTIONS APPEAR OVER HERE AFTER BEING ADDED </p>
+			<p id="result"></p>
 		</div>
 	</div>
 </body>
--- questionSelect.php
+++ questionSelect.php
@@ -1,6 +1,12 @@
 <!DOCTYPE HTML>
 <html>
-<?php include 'teacherHeader.php'; ?>
+<?php 
+include 'teacherHeader.php'; 
+$name = "mike";
+//$selectedQuestions = array('questions' => $_POST[
+
+
+?>
 <title> Edit Test </title>
 <head>
 <!-- Resize to fit screen dynamically -->
@@ -15,30 +21,48 @@
 </style>
 </head>
 <script>
+
 	function moveText(myVar) {
 	var checkBox = document.getElementById("quesCheck");
 		if(checkBox.checked == true) {
-			document.getElementById("text").innerHTML = myVar; //"hello world <br>";
+			var add = document.getElementById("selected");
+			//add.innerHTML = myVar;
+			var node = document.createElement("P");
+			var textnode = document.createTextNode(myVar);
+			node.appendChild(textnode);
+			add.appendChild(node);
+			
 		} else {
-			document.getElementById("text").innerHTML = "";
+			//document.getElementById("text").innerHTML = "Removed Question";
+			add.innerHTML = "";
 		}
 	}
+
+/*	function moveText(recvVar) {
+		var checkBox = document.getElementById("quesCheck");
+		if(checkBox.checked == true) {
+			var node = document.createElement("LI");
+			var textNode = document.createTextNode("test");
+			node.appendChild(textNode);
+			document.getElemeentById("selected").appendChild(node);
+		}
+	}
+*/
 </script>
 <body>
 	<h2> Choose Questions to Add to the Test </h2>
 	<div class="row">
 		<div class="column" style="background-color:#fff;">
 			<h2> Selected Questions </h2>
-			<p> Make selected Questions appear over here</p>
-			<p id="text"> </p>
+			<p id="selected"></p>
 		</div>
 		<div class="column" style="background-color:#bbb;">
 			<h2> Availabe Questions </h2>
 			<!-- Increase number dynamically -->
 			<!-- https://stackoverflow.com/questions/13330202/how-to-create-list-of-checkboxes-dynamically-with-javascript-->
-			<form method="get">
-				<input type="checkbox" id="quesCheck" name="question1" value='var' onclick="moveText('temp text')">Question # 1<br><br>
-				<input type="checkbox" id="quesCheck2" onclick="moveText()">Question # 2<br><br>
+			<form method="get"> <!-- GET just for testing, will be POST in submitted project -->
+				<input type="checkbox" id="quesCheck" name="question1"  value=<?php echo $name; ?> onclick="moveText('<?php echo $name; ?>');">Question #1: <?php echo $name; ?><br><br>
+				<input type="checkbox" id="quesCheck2" name="question2" value=<?php echo $name; ?> onclick="moveText('test2')">Question #2: <?php echo $name; ?><br><br>
 				<input type="submit">
 			</form>
 		<!--	<button type="button" onClick= > Submit Selected </button> -->
