4623b5a65925380c1d466afd041521b48f90628f
MichaelUlrich <mikeulrich3@gmail.com>
2018-10-16 21:06:46 -0400

    Questions can now be added to data base.  Working on selecting and saving to exam data base

--- questionSelect.php
+++ questionSelect.php
@@ -2,9 +2,14 @@
 <html>
 <?php 
 include 'teacherHeader.php'; 
-$name = "mike";
+//$name = "mike";
 //$selectedQuestions = array('questions' => $_POST[
+$ch = curl_init("https://web.njit.edu/~bkw2/fetchQuestions.php");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
 
+echo $result;
 
 ?>
 <title> Edit Test </title>
@@ -21,9 +26,8 @@ $name = "mike";
 </style>
 </head>
 <script>
-
 	function moveText(myVar) {
-	var checkBox = document.getElementById("quesCheck");
+		var checkBox = document.getElementById("quesCheck");
 		if(checkBox.checked == true) {
 			var add = document.getElementById("selected");
 			//add.innerHTML = myVar;
@@ -31,23 +35,11 @@ $name = "mike";
 			var textnode = document.createTextNode(myVar);
 			node.appendChild(textnode);
 			add.appendChild(node);
-			
 		} else {
 			//document.getElementById("text").innerHTML = "Removed Question";
 			add.innerHTML = "";
 		}
 	}
-
-/*	function moveText(recvVar) {
-		var checkBox = document.getElementById("quesCheck");
-		if(checkBox.checked == true) {
-			var node = document.createElement("LI");
-			var textNode = document.createTextNode("test");
-			node.appendChild(textNode);
-			document.getElemeentById("selected").appendChild(node);
-		}
-	}
-*/
 </script>
 <body>
 	<h2> Choose Questions to Add to the Test </h2>
@@ -60,12 +52,12 @@ $name = "mike";
 			<h2> Availabe Questions </h2>
 			<!-- Increase number dynamically -->
 			<!-- https://stackoverflow.com/questions/13330202/how-to-create-list-of-checkboxes-dynamically-with-javascript-->
-			<form method="get"> <!-- GET just for testing, will be POST in submitted project -->
+			<form method="get"> <!-- GET just for testing, will be POST in submitted
+			project -->
 				<input type="checkbox" id="quesCheck" name="question1"  value=<?php echo $name; ?> onclick="moveText('<?php echo $name; ?>');">Question #1: <?php echo $name; ?><br><br>
 				<input type="checkbox" id="quesCheck2" name="question2" value=<?php echo $name; ?> onclick="moveText('test2')">Question #2: <?php echo $name; ?><br><br>
 				<input type="submit">
 			</form>
-		<!--	<button type="button" onClick= > Submit Selected </button> -->
 		</div>
 	</div>
 </body>
