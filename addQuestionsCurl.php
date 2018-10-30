2cad5a29db74879b9d78f0408a28f0b931eef584
MichaelUlrich <mikeulrich3@gmail.com>
2018-10-30 17:42:20 -0400

    Updated addQuestionsCurl.php

--- addQuestionsCurl.php
+++ addQuestionsCurl.php
@@ -1,9 +1,7 @@
 <?php
 	//$echo 'examDbCurl: ' . $_POST['question'];
-	$question = array('question' => $_POST['question'], 'type' =>$_POST['type'], 'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'], 'points' => $_POST['points'], 'testCases' => $_POST['testCases'], 'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'], 'returnPrint' => $_POST['returnPrint']);
-
-  echo $question;
-	/*$ch = curl_init("");
+	$question = array('question' => $_POST['question'], 'type' =>$_POST['type'], 'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'], 'points' => $_POST['points'], 'testCases' => $_POST['testCases'], 'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'], 'returnPrint' => $_POST['returnPrint']);	
+	$ch = curl_init("");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $question);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
