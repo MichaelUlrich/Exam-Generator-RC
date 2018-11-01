6d6e36bb35dfb009372dc7e64ce476319601ae17
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-01 17:36:53 -0400

    Reformated for legibility

--- addQuestionsCurl.php
+++ addQuestionsCurl.php
@@ -1,6 +1,10 @@
 <?php
 	//$echo 'examDbCurl: ' . $_POST['question'];
-	$question = array('question' => $_POST['question'], 'type' =>$_POST['type'], 'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'], 'points' => $_POST['points'], 'testCases' => $_POST['testCases'], 'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'], 'returnPrint' => $_POST['returnPrint']);	
+	$question = array('question' => $_POST['question'], 'type' =>$_POST['type'],
+					'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'],
+					'points' => $_POST['points'], 'testCases' => $_POST['testCases'],
+					'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'],
+					'returnPrint' => $_POST['returnPrint']);	
 	$ch = curl_init("");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $question);
