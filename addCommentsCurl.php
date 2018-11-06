5e2c365a47f14c1f89705cc81925fe043d742937
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-06 18:42:35 -0500

    Added PHP cURL ability

--- addCommentsCurl.php
+++ addCommentsCurl.php
@@ -1,3 +1,13 @@
-<?php 
-// TODO: PHP file to send comments to graded test db
+<?php
+// TODO: PHP file to send comments to graded test
+				$commentResults = array('comment' => $_POST['comment'], 'grade' => $_POST['grade'],
+					'maxGrade' => $_POST['maxGrade'], 'studentInput' => $_POST['studentInput']);
+        $ch = curl_init(/*NEED URL*/);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$result = curl_exec($ch);
+
+	echo $
  ?>
