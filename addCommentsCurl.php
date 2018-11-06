90dd66494835fd8e15c708a565f973a92b72b839
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-06 18:49:41 -0500

    Fixed incorrect variable

--- addCommentsCurl.php
+++ addCommentsCurl.php
@@ -2,9 +2,9 @@
 // TODO: PHP file to send comments to graded test
 				$commentResults = array('comment' => $_POST['comment'], 'grade' => $_POST['grade'],
 					'maxGrade' => $_POST['maxGrade'], 'studentInput' => $_POST['studentInput']);
-        $ch = curl_init("https://web.njit.edu/~bkw2/finalComments.php");
+  $ch = curl_init("https://web.njit.edu/~bkw2/finalComments.php");
 	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $commentResults);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 	$result = curl_exec($ch);
