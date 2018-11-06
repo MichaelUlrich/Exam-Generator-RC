68b97f08b66f78a5e3d06981251177f4d6fac9cc
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-06 17:19:57 -0500

    Updated data passed via cURL request

--- testCurl.php
+++ testCurl.php
@@ -1,10 +1,10 @@
 <?php
 	// TODO: PHP file to send student input for grading
-	$testData = array('question' => $_POST['question'], 'type' =>$_POST['type'],
-					'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'],
+	$testData = array('question' => $_POST['question'], 'id' =>$_POST['id']);
+				/*	'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'],
 					'points' => $_POST['points'], 'testCases' => $_POST['testCases'],
 					'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'],
-					'returnPrint' => $_POST['returnPrint'], 'studentInput' => $_POST['studentInput']);
+					'returnPrint' => $_POST['returnPrint'], 'studentInput' => $_POST['studentInput']);*/
 	$ch = curl_init("https://web.njit.edu/~bkw2/grader.php");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
