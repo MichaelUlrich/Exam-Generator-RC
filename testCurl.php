de3cb2a5647d5fbed5f1b91651e05eb7bdc4a684
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-06 16:12:46 -0500

    Added PHP cURL ability

--- testCurl.php
+++ testCurl.php
@@ -1,8 +1,10 @@
 <?php
 	// TODO: PHP file to send student input for grading
-	$testData = array('studentCode' => $_POST['studentCode'], 'answer' => $_POST['answer']);
-	//echo $_POST['studentCode'];
-	//echo $_POST['answer'];
+	$testData = array('question' => $_POST['question'], 'type' =>$_POST['type'],
+					'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'],
+					'points' => $_POST['points'], 'testCases' => $_POST['testCases'],
+					'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'],
+					'returnPrint' => $_POST['returnPrint']);
 	$ch = curl_init("https://web.njit.edu/~bkw2/grader.php");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
