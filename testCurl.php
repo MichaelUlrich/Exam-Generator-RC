0fb2751dd098ad7867a6faefc82d388d922ca4b7
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-06 16:24:20 -0500

    Added studentInput field to cURL request

--- testCurl.php
+++ testCurl.php
@@ -4,7 +4,7 @@
 					'loopType' => $_POST['loopType'], 'difficulty' => $_POST['diff'],
 					'points' => $_POST['points'], 'testCases' => $_POST['testCases'],
 					'functionName' => $_POST['functionName'], 'variableNames' => $_POST['variableNames'],
-					'returnPrint' => $_POST['returnPrint']);
+					'returnPrint' => $_POST['returnPrint'], 'studentInput' => $_POST['studentInput']);
 	$ch = curl_init("https://web.njit.edu/~bkw2/grader.php");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
