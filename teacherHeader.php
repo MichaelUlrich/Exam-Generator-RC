3e72aff67f1c613f998c32d5548e287d2ce46870
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-07 13:18:41 -0500

    Set to always True for testing

--- teacherHeader.php
+++ teacherHeader.php
@@ -1,11 +1,16 @@
 <?php
 	/* Header to include in all Teacher HTML&PHP files to ensure only an instructor can access test creation*/
 	session_start();
-	if(!isset($_SESSION['teacher']) || $_SESSION['teacher'] != true) {
+	/*
+	if(!isset($_SESSION['teacher']) || $_SESSION['teacher'] != true)
+		WILL BE TRUE FOR TESTING
+	*/
+	if(true) { 
 		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator-RC/login.html");
 		die();
 	} else {
 		echo "Hello " . $_SESSION['username'] ;
+		echo "[TESTING W/HEADER == TRUE]";
 	}
 
 ?>
