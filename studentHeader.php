0f90340ab0c1b66e6f2484509e983c13e5602de9
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-07 13:02:12 -0500

    Update URL for re-directing

--- studentHeader.php
+++ studentHeader.php
@@ -2,7 +2,7 @@
 	/* Header to include in all Student HTML&PHP files to ensure a valid student is logged in to access tests and grades */
 	session_start();
 if(!isset($_SESSION['student']) || $_SESSION['student'] != true) {
-		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator/login.html");
+		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator-RC/login.html");
 		die();
 	} else {
 		echo "[Student mode is always enabled for testing]" . "<br>";
