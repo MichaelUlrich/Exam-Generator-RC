c23e64003b2314534e82881d2f630f778a9f4b24
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-07 13:04:24 -0500

    Updated URL for re-directing

--- teacherHeader.php
+++ teacherHeader.php
@@ -2,7 +2,7 @@
 	/* Header to include in all Teacher HTML&PHP files to ensure only an instructor can access test creation*/
 	session_start();
 	if(!isset($_SESSION['teacher']) || $_SESSION['teacher'] != true) {
-		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator/login.html");
+		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator-RC/login.html");
 		die();
 	} else {
 		echo "Hello " . $_SESSION['username'] ;
