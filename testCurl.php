f8edd03d65c95d5b151191ce82a72e213fee55db
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-08 15:18:55 -0500

    Changed variable name for cURL

--- testCurl.php
+++ testCurl.php
@@ -1,6 +1,6 @@
 <?php
 	// TODO: PHP file to send student input for grading
-	$testData = array('question' => $_POST['question'], 'username' => $_POST['username']);
+	$testData = array('studentInput' => $_POST['studentInput'], 'username' => $_POST['username']);
 	$ch = curl_init("https://web.njit.edu/~bkw2/grader.php");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
