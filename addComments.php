4d0890d9ec04359e03c62dab5f291da09fa3cc50
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-12 14:18:35 -0500

    Re-added temp values for testing

--- addComments.php
+++ addComments.php
@@ -14,11 +14,11 @@ tr:nth-child(even){background-color: #bbb;}
 //th{left; padding: 16px;background-color: #f2f2f2; border: 1px solid grey}
 </style>
 <script>
-/*var sample = [{"studentInput":"input", "autoComments":"Missing Function name, Wrong Return Type", "grade":"100", "maxGrade": "100"},
+var sample = [{"studentInput":"input", "autoComments":"Missing Function name, Wrong Return Type", "grade":"100", "maxGrade": "100"},
 					{"studentInput":"input2", "autoComments":"autoComment2", "grade":"100", "maxGrade": "100"},
 					{"studentInput":"input3", "autoComments":"autoComment3", "grade":"100", "maxGrade": "100"},
 					{"studentInput":"input4", "autoComments":"autoComment4", "grade":"100", "maxGrade": "100"},
-					{"studentInput":"input5", "autoComments":"autoComment5", "grade":"100", "maxGrade": "100"}];*/
+					{"studentInput":"input5", "autoComments":"autoComment5", "grade":"100", "maxGrade": "100"}];
 function drawAutoComments() {
 		var inputTd, idTd, idText, inputText, commentTd, commentText,
 					gradeTd, gradeText, tr, tableBody, intI, editTd, editText,
