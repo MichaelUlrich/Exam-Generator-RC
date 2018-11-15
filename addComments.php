709b0637a7401f394f4a890d6aad6711fb9dac07
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-15 11:56:46 -0500

    viewing ID

--- addComments.php
+++ addComments.php
@@ -71,9 +71,9 @@ function ajaxGetRequest(student) {
 	var xmhlObj = new XMLHttpRequest();
 	var username = studentId.value;
 	//studentId = studentId.value; //ID to send to db, pull Answers w/ matching UCID
-	document.getElementById("testing").innerHTML = "sending: "+ username;
+	document.getElementById("testing").innerHTML = "sending: "+ studentId;
 	var url = "username="+username;
-	document.getElementById("testing").innerHTML = url;
+//	document.getElementById("testing").innerHTML = url;
 	var text, responseJSON;
 	xmhlObj.open("POST", phpFile, true);
 	xmhlObj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //Sending URL encoded variables
