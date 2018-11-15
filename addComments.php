3a50ccc75df1f26d0d3cb65b2d60d2cd6e82ceb8
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-15 11:15:34 -0500

    Changed variable names

--- addComments.php
+++ addComments.php
@@ -71,14 +71,14 @@ function ajaxGetRequest(student) {
 	var xmhlObj = new XMLHttpRequest();
 	studentId = studentId.value; //ID to send to db, pull Answers w/ matching UCID
 	var url = "username="+studentId;
-	var responseText, responseJSON;
+	var text, responseJSON;
 	xmhlObj.open("POST", phpFile, true);
 	xmhlObj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //Sending URL encoded variables
 	xmhlObj.onreadystatechange = function() {
 	if(xmhlObj.readyState == 4 && xmhlObj.status == 200) {  //Conection is established and working
 		document.getElementById("nameTesting").innerHTML = "called cURL";
-		responseText = xmhlObj.responseText; //Returns student input for specific UCID
-		responseJSON = JSON.parse(responseText);
+		text = xmhlObj.responseText; //Returns student input for specific UCID
+		responseJSON = JSON.parse(text);
 		drawAutoComments(responseJSON);
 		}
 	}
