a7fdb7ab341009bf67b1641cedbe4f770e9af646
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-14 23:35:24 -0500

    Fixed URL

--- addCommentsGetCurl.php
+++ addCommentsGetCurl.php
@@ -1,7 +1,7 @@
 <?php
 // TODO: PHP file to get auto-grader comments for review
   $testData = array('username' => $_POST['username']);//, 'username' => $_POST['username'])
-  $ch = curl_init("https://web.njit.edu/~bkw2/getQuestionsByUser.php");
+  $ch = curl_init("https://web.njit.edu/~bkw2/getQuestionsbyUser.php");
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
