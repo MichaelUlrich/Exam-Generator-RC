74bae861570d25b4a2c4861a1e31bea45af3d951
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-14 21:07:13 -0500

    Pull usernames of students for selecting questions

--- addCommentsGetUsers.php
+++ addCommentsGetUsers.php
@@ -1,8 +1,8 @@
 <?php
-// TODO: PHP file to get auto-grader comments for review
-  $ch = curl_init(/*"https://web.njit.edu/~bkw2/getExam.php"*/);
+// TODO: PHP file to get student usernames
+  $ch = curl_init("https://web.njit.edu/~bkw2/getUsername.php");
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
-    echo json_encod($result);
+    echo json_encode($result);
 ?>
