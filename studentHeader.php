b0d48427edba264bdbab79bc82236339ab3f848d
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-15 14:15:28 -0500

    Updated headers to no longer be in testing states

--- studentHeader.php
+++ studentHeader.php
@@ -1,15 +1,7 @@
 <?php
 	/* Header to include in all Student HTML&PHP files to ensure a valid student is logged in to access tests and grades */
 session_start();
-/*
 if(!isset($_SESSION['student']) || $_SESSION['teacher'] != true)
-	WILL BE TRUE FOR TESTING
-*/
-if(!true) {
 		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator-RC/login.html");
 		die();
-	} else {
-		//echo "[Student mode is always enabled for testing]" . "<br>";
-		echo $_SESSION['username'];
-	}
 ?>
--- teacherHeader.php
+++ teacherHeader.php
@@ -1,16 +1,7 @@
 <?php
 	/* Header to include in all Teacher HTML&PHP files to ensure only an instructor can access test creation*/
 	session_start();
-	/*
-	if(!isset($_SESSION['teacher']) || $_SESSION['teacher'] != true)
-		WILL BE TRUE FOR TESTING
-	*/
-	if(!true) {
+	if(!isset($_SESSION['teacher']) || $_SESSION['teacher'] != true
 		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator-RC/login.html");
 		die();
-	} else {
-		echo "Hello " . $_SESSION['username'] ;
-		echo "[TESTING W/HEADER == TRUE]";
-	}
-
 ?>
