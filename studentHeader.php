1efcb4c97980f9d658c46487d6fe3fda99094a75
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-15 14:48:28 -0500

    Fixed incorrect variable used

--- studentHeader.php
+++ studentHeader.php
@@ -1,7 +1,7 @@
 <?php
 	/* Header to include in all Student HTML&PHP files to ensure a valid student is logged in to access tests and grades */
 session_start();
-if(!isset($_SESSION['student']) || $_SESSION['teacher'] != true) {
+if(!isset($_SESSION['student']) || $_SESSION['student'] != true) {
 		header("Location: https://web.njit.edu/~meu3/CS490/Exam-Generator-RC/login.html");
 		die();
 	}
