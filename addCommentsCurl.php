35a82f3ccbddfdd245ebb4d1f88b3bae97663573
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-15 12:24:20 -0500

    Added username to cURL

--- addCommentsCurl.php
+++ addCommentsCurl.php
@@ -1,7 +1,7 @@
 <?php
 // TODO: PHP file to send comments to graded test
 				$commentResults = array('comment' => $_POST['comment'], 'grade' => $_POST['grade'],
-					'maxGrade' => $_POST['maxGrade'], 'studentInput' => $_POST['studentInput']);
+					'maxGrade' => $_POST['maxGrade'], 'studentInput' => $_POST['studentInput'], 'username'=> $_POST['username']);
   $ch = curl_init("https://web.njit.edu/~bkw2/finalComments.php");
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $commentResults);
