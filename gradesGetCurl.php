38ede2ddb532f8875c213cb673465761e467b7c8
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-11-14 22:17:26 -0500

    no longer JSON encoding

--- gradesGetCurl.php
+++ gradesGetCurl.php
@@ -1,3 +1,8 @@
 <?php
 // TODO: PHP file to get grades and comments to display to student
+  $ch = curl_init(/*"https://web.njit.edu/~bkw2/getExam.php"*/);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($ch);
+  echo json_encod($result);
  ?>
